11579c87261ade30271b27280641aed84e90e66b
Stuart <stuart@planetcoops.com>
2022-12-01 11:40:06 +0000

    Replace unsafe Lombok DATA annotation with safe alternatives

--- src/main/java/guru/springfamework/domain/Category.java
+++ src/main/java/guru/springfamework/domain/Category.java
@@ -1,16 +1,21 @@
 package guru.springfamework.domain;
 
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 /**
  * Created by jt on 9/24/17.
  */
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 public class Category {
 
@@ -19,4 +24,16 @@ public class Category {
     private Long id;
     private String name;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Category category = (Category) o;
+        return id != null && Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
